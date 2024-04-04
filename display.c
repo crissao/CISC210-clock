2d0f6885d5070b07305cddf1836571c52cd6e945
Crissa Owens <crissao@udel.edu>
2024-04-04 19:18:49 -0400

    display_colons() function in display.c done

--- display.c
+++ display.c
@@ -1,7 +1,12 @@
 #include "display.h" 
 
-int open_display(void){
+#define WHITE 0xFFFF
+ 
 
+int open_display(void){
+	pi_framebuffer_t *fb=getFrameBuffer();
+    	sense_fb_bitmap_t *bm=fb->bitmap;
+	return 0;
 }
 
 void display_time(int hours, int minutes, int seconds){
@@ -9,7 +14,23 @@ void display_time(int hours, int minutes, int seconds){
 }
 
 void display_colons(void){
-
+	sense_fb_bitmap_t *bm;
+	bm->pixel[1][2]=WHITE;
+        bm->pixel[1][3]=WHITE;
+	bm->pixel[2][2]=WHITE;
+        bm->pixel[2][3]=WHITE;
+	bm->pixel[4][2]=WHITE;
+        bm->pixel[4][3]=WHITE;
+	bm->pixel[5][2]=WHITE;
+        bm->pixel[5][3]=WHITE;
+	bm->pixel[1][5]=WHITE;
+        bm->pixel[1][6]=WHITE;
+	bm->pixel[2][5]=WHITE;
+        bm->pixel[2][6]=WHITE;
+	bm->pixel[4][5]=WHITE;
+        bm->pixel[4][6]=WHITE;
+	bm->pixel[5][5]=WHITE;
+        bm->pixel[5][6]=WHITE;
 }
 
 void display_hours(int hours){
@@ -27,3 +48,4 @@ void display_seconds(int seconds){
 void close_display(void){
 
 }
+
