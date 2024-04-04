b70c2823bbeb284f812612fd9be6ca92a220c319
Crissa Owens <crissao@udel.edu>
2024-04-04 19:37:23 -0400

    updated main

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include "display.h"
 
-int main(){
-	return 0
+int main(void){
+	int hours,minutes,seconds;
+	scanf("%d:%d:%d",&hours,&minutes,&seconds);
+	open_display();
+	while(hours && minutes && seconds){
+		display_time(hours,minutes,seconds);
+		scanf("%d:%d:%d",&hours,&minutes,&seconds);
+	}
+	close_display();
+	return 0;
 }
