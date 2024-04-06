e2ad6ba25216e43f7216640f820b65b07f451fd3
Crissa Owens <crissao@udel.edu>
2024-04-06 13:20:40 -0400

    joystick prototypes added

--- display.h
+++ display.h
@@ -3,17 +3,17 @@
 #include <unistd.h>
 #include "sense.h"
 
+//joystick
+void allocate_fb(void);
+void free_fb(void);
+void display_dot(char x, char y);
+
+//clock
 int open_display(void);
-
 void display_time(int, int, int);
-
 void display_colons(void);
-
 void display_hours(int);
-
 void display_minutes(int);
-
 void display_seconds(int);
-
 void close_display(void);
 
